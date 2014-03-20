570a72a510504574899e4d7979d855c034518569
Finn Ellis <relsqui@chiliahedron.com>
2014-03-20 02:50:59 -0700

    Don't show pending quotes.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,17 @@
+from datetime import datetime
+
 from app import db
+
 
 class Quotes(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    quote = db.Column(db.Text)
+    by = db.Column(db.String(255))
+    date = db.Column(db.DateTime, default = datetime.now())
+    comment = db.Column(db.Text, default = "")
+    pending = db.Column(db.Boolean, default = True)
     upvotes = db.Column(db.Integer, default = 0)
     downvotes = db.Column(db.Integer, default = 0)
-    quote = db.Column(db.Text)
     bayesian = db.Column(db.Float, default = 0)
 
     def __repr__(self):
--- app/views.py
+++ app/views.py
@@ -5,21 +5,23 @@ from app import app, forms, db
 from app.models import Quotes
 from config import POSTS_PER_PAGE
 
+
 def render_quotes(query, source, page = 1):
     quotes = query.paginate(page, POSTS_PER_PAGE, False)
     return render_template("quotes.html", source = source, quotes = quotes)
+
 
 @app.route("/newest")
 @app.route("/page/<int:page>")
 @app.route("/")
 def home(page = 1):
-    query = Quotes.query.order_by(desc(Quotes.id))
+    query = Quotes.query.filter_by(pending = False).order_by(desc(Quotes.id))
     return render_quotes(query, "home", page)
 
 @app.route("/top")
 @app.route("/top/page/<int:page>")
 def top(page = 1):
-    query = Quotes.query.order_by(desc(Quotes.bayesian))
+    query = Quotes.query.filter_by(pending = False).order_by(desc(Quotes.bayesian))
     return render_quotes(query, "top", page)
 
 @app.route("/about")
@@ -30,13 +32,14 @@ def about():
 def submit():
     form = forms.QuoteSubmission()
     if form.validate_on_submit():
-        quote = Quotes(quote = form.quote.data)
+        quote = Quotes(quote = form.quote.data, by = form.submitter.data)
         db.session.add(quote)
         db.session.commit()
         db.session.refresh(quote)
         flash("Submitted quote #{}".format(quote.id))
         return redirect("/")
     return render_template("submit.html", form = form)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
