11943d50248d955a2e5ac97f2927ee473f569f4f
Finn Ellis <relsqui@chiliahedron.com>
2014-03-20 19:26:47 -0700

    Implement top recent quotes page.

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,10 @@
 from flask.ext.wtf import Form
-from wtforms import TextAreaField, StringField
+from wtforms import TextAreaField, StringField, IntegerField
 from wtforms.validators import Required
 
 class QuoteSubmission(Form):
     quote = TextAreaField("quote", validators = [Required()])
     submitter = StringField("submitter")
+
+class DateRange(Form):
+    days = IntegerField("days", default = 14)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect
+from datetime import datetime, timedelta
+from flask import render_template, flash, redirect, url_for
 from sqlalchemy import desc
 
 from app import app, forms, db
@@ -10,6 +11,9 @@ def render_quotes(query, source, page = 1):
     quotes = query.paginate(page, POSTS_PER_PAGE, False)
     return render_template("quotes.html", source = source, quotes = quotes)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/newest")
 @app.route("/page/<int:page>")
@@ -22,11 +26,23 @@ def home(page = 1):
 @app.route("/top/page/<int:page>")
 def top(page = 1):
     query = Quotes.query.filter_by(pending = False).order_by(desc(Quotes.bayesian))
+
     return render_quotes(query, "top", page)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/recent", methods = ['GET', 'POST'])
+@app.route("/recent/page/<int:page>", methods = ['GET', 'POST'])
+@app.route("/recent/days/<int:days>", methods = ['GET', 'POST'])
+@app.route("/recent/days/<int:days>/page/<int:page>", methods = ['GET', 'POST'])
+def recent(page = None, days = None):
+    form = forms.DateRange()
+    if form.validate_on_submit():
+        return redirect(url_for("recent", days = form.days.data, page = page))
+    if not days:
+        days = form.days.default
+    if not page:
+        page = 1
+    quotes = Quotes.query.filter_by(pending = False).filter(Quotes.date > datetime.now() - timedelta(days)).order_by(desc(Quotes.bayesian)).paginate(page, POSTS_PER_PAGE, False)
+    return render_template("recent.html", form = form, days = days, source = "recent", quotes = quotes);
 
 @app.route("/submit", methods = ['GET', 'POST'])
 def submit():
